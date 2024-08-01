2a10898eb449c9ad9599334d4b9e9024c6f1d792
Alfonso Mulsa <amulsa@gmail.com>
2024-08-01 10:59:10 +0200

    Commit inicial

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,22 @@
 import { NgModule } from '@angular/core';
+import { AppRoutingModule } from './app-routing.module';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { BrowserModule } from '@angular/platform-browser';
 
-import { AppRoutingModule } from './app-routing.module';
+import { SharedModule } from './shared/shared.module';
+
 import { AppComponent } from './app.component';
+
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
+    AppRoutingModule,
+    BrowserAnimationsModule,
     BrowserModule,
-    AppRoutingModule
+    SharedModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/shared/components/menu/menu.component.ts
@@ -0,0 +1,20 @@
+import { Component } from '@angular/core';
+import { MenuItem } from 'primeng/api';
+
+@Component({
+  selector: 'shared-menu',
+  templateUrl: './menu.component.html',
+  styles: ``
+})
+export class MenuComponent {
+
+  public menuItems: MenuItem[] = [];
+
+    ngOnInit() {
+        this.menuItems = [
+            { label: 'New', icon: 'pi pi-plus' },
+            { label: 'Open', icon: 'pi pi-download' },
+            { label: 'Undo', icon: 'pi pi-refresh' },
+        ];
+    }
+}
